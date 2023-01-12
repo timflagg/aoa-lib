935543f75d13e9d20baade9d3fbfc210e9157fb8
Alex <alexbrucely@gmail.com>
2023-01-11 22:41:30 -0800

    bump

---
+++ aoa-cluster1/4-gloo-mesh/test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+./tools/wait-for-rollout.sh deployment ext-auth-service gloo-mesh-addons 10 ${cluster_context}
+./tools/wait-for-rollout.sh deployment rate-limiter gloo-mesh-addons 10 ${cluster_context}
+./tools/wait-for-rollout.sh deployment redis gloo-mesh-addons 10 ${cluster_context}
+
---
+++ notes.md
@@ -0,0 +1,3 @@
+./aoa-tools/deploy.sh $GM_LICENSE_KEY 2.1.2 aoa-mgmt m1 mgmt mgmt ably77 aoa-lib HEAD
+./aoa-tools/deploy.sh $GM_LICENSE_KEY 2.1.2 aoa-cluster1 m1 cluster1 mgmt ably77 aoa-lib HEAD
+./aoa-tools/deploy.sh $GM_LICENSE_KEY 2.1.2 aoa-cluster2 m1 cluster2 mgmt ably77 aoa-lib HEAD
