ece15a88b8bbd3568ab54a9e877169896c0cee92
Alex <alexbrucely@gmail.com>
2023-02-01 20:31:48 -0800

    fix/refactor output in test scripts

--- environments/gloo-edge-httpbin-bookinfo/3-httpbin-config/test.sh
+++ environments/gloo-edge-httpbin-bookinfo/3-httpbin-config/test.sh
@@ -3,18 +3,23 @@
 echo 
 echo "installation complete:"
 echo
-echo "To access argocd dashboard, follow the methods below:"
+echo "To access applications, follow the methods below:"
 echo 
-echo "Method 1: modify /etc/hosts on your local machine (this will require sudo privileges)"
+echo "Method 1 - LoadBalancer External-IP: modify /etc/hosts on your local machine (this will require sudo privileges)"
 echo "cat <<EOF | sudo tee -a /etc/hosts"
 echo "$(kubectl -n gloo-system get service gateway-proxy -o jsonpath='{.status.loadBalancer.ingress[0].ip}') argocd-local.glootest.com bookinfo-local.glootest.com httpbin-local.glootest.com"
+echo "EOF"
+echo
+echo "Method 2 - K3d LB Integration: modify /etc/hosts on your local machine (this will require sudo privileges)"
+echo "cat <<EOF | sudo tee -a /etc/hosts"
+echo "localhost argocd-local.glootest.com bookinfo-local.glootest.com httpbin-local.glootest.com"
 echo "EOF"
 echo
 echo "access argocd at https://argocd-local.glootest.com/argo"
 echo "access the bookinfo application at: https://bookinfo-local.glootest.com/productpage"
 echo "access the httpbin application at: https://httpbin-local.glootest.com/get"
 echo
-echo "Method 2: use port-forwarding"
+echo "Method 3: use port-forwarding"
 echo "alternatively, access argocd using port-forward command:" 
 echo "kubectl port-forward svc/argocd-server -n argocd 9999:443"
 echo
--- environments/gm-single-cluster-freestyle/5-gloo-mesh-config/test.sh
+++ environments/gm-single-cluster-freestyle/5-gloo-mesh-config/test.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
+ISTIO_REVISION="1-15"
+
 echo 
 echo "installation complete:"
 echo
-echo "To access argocd dashboard, follow the methods below:"
+echo "To access applications, follow the methods below:"
 echo 
-echo "Method 1: modify /etc/hosts on your local machine (this will require sudo privileges)"
+echo "Method 1 - LoadBalancer External-IP: modify /etc/hosts on your local machine (this will require sudo privileges)"
 echo "cat <<EOF | sudo tee -a /etc/hosts"
-echo "$(kubectl -n gloo-system get service gateway-proxy -o jsonpath='{.status.loadBalancer.ingress[0].ip}') argocd-local.glootest.com gmui-local.glootest.com"
+echo "$(kubectl -n istio-gateways get service istio-ingressgateway-${ISTIO_REVISION} -o jsonpath='{.status.loadBalancer.ingress[0].ip}') argocd-local.glootest.com gmui-local.glootest.com"
+echo "EOF"
+echo
+echo "Method 2 - K3d LB Integration: modify /etc/hosts on your local machine (this will require sudo privileges)"
+echo "cat <<EOF | sudo tee -a /etc/hosts"
+echo "localhost argocd-local.glootest.com gmui-local.glootest.com"
 echo "EOF"
 echo
 echo "access argocd at https://argocd-local.glootest.com/argo"
 echo "access Gloo Mesh UI at https://gmui-local.glootest.com"
 echo
-echo "Method 2: use port-forwarding"
+echo "Method 3: use port-forwarding"
 echo "alternatively, access argocd using port-forward command:" 
 echo "kubectl port-forward svc/argocd-server -n argocd 9999:443"
 echo
